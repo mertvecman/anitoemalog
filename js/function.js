71bc1cba447f295720a5aa2fbf087f84210209d3
mertvecman <kajaha127@gmail.com>
2023-06-10 22:06:10 +0900

    최종 완성 (메뉴 활성화 제외)

--- js/function.js
+++ js/function.js
@@ -1,24 +1,24 @@
 // 네비게이션바
-$(function () {
-  const $mnu = $("header > nav > .gnb > ol > li > a");
+// $(function () {
+//   const $mnu = $("header > nav > .gnb > .indicator > li > a");
 
-  const arrTopVal = [];
-  $("article").each(function (idx) {
-    arrTopVal[idx] = $(this).offset().top;
-  });
+//   const arrTopVal = [];
+//   $(".page").each(function (idx) {
+//     arrTopVal[idx] = $(this).offset().top;
+//   });
 
-  $(window).on("scroll", function () {
-    let scrollTop = Math.ceil($(this).scrollTop());
+//   $(window).on("scroll", function () {
+//     let scrollTop = Math.ceil($(this).scrollTop());
 
-    for (let i = 0; i < $mnu.length; i++) {
-      if (scrollTop >= arrTopVal[i] - 50) {
-        $mnu.eq(i).parent().addClass("on").siblings().removeClass("on");
-      } else if (scrollTop < arrTopVal[0] - 50) {
-        $mnu.parent().removeClass("on");
-      }
-    }
-  });
-});
+//     for (let i = 0; i < $mnu.length; i++) {
+//       if (scrollTop >= arrTopVal[i] - 50) {
+//         $mnu.eq(i).parent().addClass("on").siblings().removeClass("on");
+//       } else if (scrollTop < arrTopVal[0] - 50) {
+//         $mnu.parent().removeClass("on");
+//       }
+//     }
+//   });
+// });
 
 // 스와이퍼
 const swiper1 = new Swiper(".swiper", {
