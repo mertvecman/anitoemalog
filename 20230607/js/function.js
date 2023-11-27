ddad3d7d01339c7a05049f2981f3fd903c64a517
Kim Jinhyuk <134856311+mertvecman@users.noreply.github.com>
2023-11-28 03:11:35 +0900

    Add files via upload

--- 20230607/js/function.js
+++ 20230607/js/function.js
@@ -1,27 +1,36 @@
 // 네비게이션바
-// $(function () {
-//   const $mnu = $("header > nav > .gnb > .indicator > li > a");
+$(function () {
+  const $nav = $("header > nav");
+  const $mnu = $nav.find(".gnb > li > a");
 
-//   const arrTopVal = [];
-//   $(".page").each(function (idx) {
-//     arrTopVal[idx] = $(this).offset().top;
-//   });
+  // 메뉴 클릭시 이동
+  $mnu.click(function (evt) {
+    preventDefault(evt);
+    $("html, body").animate({ scrollTop: $(this.hash).offset.top });
+  });
 
-//   $(window).on("scroll", function () {
-//     let scrollTop = Math.ceil($(this).scrollTop());
+  // 배열구성
+  const arrTopVal = [];
+  $(".sect").each(function (idx) {
+    arrTopVal[idx] = $(this).offset().top;
+  });
 
-//     for (let i = 0; i < $mnu.length; i++) {
-//       if (scrollTop >= arrTopVal[i] - 50) {
-//         $mnu.eq(i).parent().addClass("on").siblings().removeClass("on");
-//       } else if (scrollTop < arrTopVal[0] - 50) {
-//         $mnu.parent().removeClass("on");
-//       }
-//     }
-//   });
-// });
+  // 스크롤
+  $(window).on("scroll", function () {
+    let scrollTop = Math.ceil($(this).scrollTop());
 
-// 스와이퍼
-const swiper1 = new Swiper(".swiper", {
+    for (let i = 0; i < $mnu.length; i++) {
+      if (scrollTop >= arrTopVal[i] - 50) {
+        $mnu.eq(i).parent().addClass("on").siblings().removeClass("on");
+      } else if (scrollTop < arrTopVal[0] - 50) {
+        $mnu.parent().removeClass("on");
+      }
+    }
+  });
+});
+
+// 스틸컷 스와이퍼(1)
+const swipepStill = new Swiper(".swiper-1", {
   slidesPerView: 1,
   spaceBetween: 10,
   autoplay: {
@@ -30,8 +39,20 @@ const swiper1 = new Swiper(".swiper", {
   },
   loop: true,
 
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true,
+  effect: "cards",
+  cards: { perSlideOffset: 3 },
+});
+
+// 캐릭터 스와이퍼(2)
+const swipepChar = new Swiper(".swiper-2", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false,
   },
+  loop: true,
+
+  effect: "fade",
+  fadeEffect: { crossFade: true },
 });
